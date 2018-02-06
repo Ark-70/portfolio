c60160d3c3ae0b4913c341c9918a09eccdb127a1
David Traparic <david.traparic@etu.univ-poitiers.fr>
2018-02-06 11:36:46 +0100

    séparation section/blocs

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -4,10 +4,14 @@ $(function() {
 let Engine = Matter.Engine,
     Render = Matter.Render,
     World = Matter.World,
+    Constraint = Matter.Constraint,
+    MouseConstraint = Matter.MouseConstraint,
+	Mouse = Matter.Mouse,
     Bodies = Matter.Bodies;
 
 // create an engine
-let engine = Engine.create();
+let engine = Engine.create(),
+	world = engine.world;
 
 // create a renderer
 let render = Render.create({
@@ -19,6 +23,26 @@ let render = Render.create({
 let boxA = Bodies.rectangle(400, 200, 80, 80);
 let boxB = Bodies.rectangle(450, 50, 80, 80);
 let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+
+// add mouse control
+    var mouse = Mouse.create(render.canvas),
+        mouseConstraint = MouseConstraint.create(engine, {
+            mouse: mouse,
+            constraint: {
+                // allow bodies on mouse to rotate
+                angularStiffness: 0.3,
+                render: {
+                    visible: false
+                }
+            }
+        });
+
+    World.add(world, mouseConstraint);
+
+// keep the mouse in sync with rendering
+render.mouse = mouse;
+
 
 // add all of the bodies to the world
 World.add(engine.world, [boxA, boxB, ground]);
--- index.php
+++ index.php
@@ -1,15 +1,44 @@
 <?php  ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
   <meta charset="UTF-8">
   <title>portfolio </title>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="assets/js/matter.min.js"></script>
-  <script src="assets/js/sketch.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/style.css">
+
 </head>
 <body>
+	<header>
+		<h1>David</h1>
+		<h1>Traparic</h1>
+		<!-- <canvas id="mattercanvas"></canvas> -->
+	</header>
+		
+		<section id="path">
+			<div class="container-fluid">
+				<h2>Mon parcours :</h2>
+				<p>Lorem ibsum ma bite</p>
+			</div>
+		</section>
 
+		<section id="skills">
+			<div class="container-fluid">
+				<h2>Mes compétences :</h2>
+				<canvas></canvas>
+			</div>
+		</section>
+		
+		<section id="creations">
+			<div class="container-fluid">
+				<p>Lorem ibsum ma bite</p>
+			</div>
+		</section>
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="assets/js/matter.min.js"></script>
+	<!-- <script src="assets/js/sketch.js"></script> -->
 </body>
 </html>
