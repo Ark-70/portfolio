9b7b17f1522db19c17f5f031674ad4889f709210
Ark-70 <david.traparic@etu.univ-poitiers.fr>
2018-02-28 16:32:25 +0100

    tout colorés

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <!--
 
-J'ai oublié de push le dernier cours de portfolio en Louet FML
-
-Je dois récupérer :
-le soulignement animé des titres des sections
-un fix du background
+Site trop stylé
+https://iamvdo.me/blog/la-cascade-css-inherit-et-initial
 
  -->
 
@@ -38,7 +35,20 @@ un fix du background
 	<section class="skills">
 		<div class="container-fluid">
 			<h2><span class="underline">Mes compétences :</span></h2>
-			<canvas></canvas>
+      <div class="skills__barcontainer">
+        <div class="progress vertical">
+            <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 40%;"></div>
+        </div>
+        <div class="progress vertical">
+            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
+        </div>
+          <div class="progress vertical">
+            <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"></div>
+        </div>
+        <div class="progress vertical">
+            <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 95%;"></div>
+        </div>
+      </div>
 		</div>
 	</section>
 
@@ -48,14 +58,14 @@ un fix du background
         <p>À travers différents projets, j'ai pu améliorer certains aspects de mes compétences. Voici une liste non exhaustive des projets que j'ai pu réalisés :</p>
         <h4>Programmation :</h4>
         <ul class="creations__liste creations__liste--prog">
-          <li>SpeedUp <em class="creations__langage">-Python puis Javascript</em></li>
-          <li>Draw me a Ship <em class="creations__langage">-Python puis Java</em></li>
-          <li>Mastermind <em class="creations__langage">-Javascript + ES6</em></li>
-          <li>A Normal Day <em class="creations__langage">-Unity/C#</em></li>
+          <li><span class="creations__projet creations__projet--speedup"><label for="speeduplink">SpeedUp</label></span> <em class="creations__langage">-Python puis Javascript</em></li>
+          <li><span class="creations__projet creations__projet--dmas">Draw me a Ship</span> <em class="creations__langage">-Python puis Java</em></li>
+          <li><span class="creations__projet creations__projet--master">Mastermind</span> <em class="creations__langage">-Javascript + ES6</em></li>
+          <li><span class="creations__projet creations__projet--day">A Normal Day</span> <em class="creations__langage">-Unity/C#</em></li>
         </ul>
         <h4>Développement web :</h4>
         <ul class="creations__liste creations__liste--dev">
-          <li>Galerie d'images éditable <em class="creations__langage">-PhP + Laravel</em></li>
+          <li><span class="creations__projet creations__projet--galimg">Galerie d'images éditable</span> <em class="creations__langage">-PhP + Laravel</em></li>
           <li>Site vitrine promotionnel<em class="creations__langage">-Sass + Mobile First</em></li>
           <li>Ce portfolio ! <em class="creations__langage"></em></li>
           <li>Un petit peu de wordpress ne ferait pas de mal<em class="creations__langage"></em></li>
@@ -63,30 +73,30 @@ un fix du background
       </article>
 
       <article class="creations__accordeon">
-        <h4 class="accordeon__titre accordeon__titre--selected">SpeedUp <a href=""><i class="fas-fa github"></i></a></h4>
+        <h4 class="accordeon__titre accordeon__titre--selected"><span class="creations__projet creations__projet--speedup">SpeedUp</span><a href=""><i class="fas-fa github"></i></a></h4>
         <div class="accordeon__contentbox accordeon__contentbox--open">
           <div class="accordeon__desc">
-            <p>Destiné à la base à l'apprentissage du clavier par les enfants, SpeedUp a été créé comme premier exercice de programmation lors de ma spécialité <abbr title="Informatique & Sciences du Numérique">ISN</abbr> au Bac. Il a été plus tard adapté et amélioré par mes soins pour pouvoir être joué directement dans le navigateur grâce à Javascript.</p>
+            <p>Destiné à la base à l'apprentissage du clavier par les enfants, <span class="creations__projet creations__projet--speedup">SpeedUp</span> a été créé comme premier exercice de programmation lors de ma spécialité <abbr title="Informatique & Sciences du Numérique">ISN</abbr> au Bac. Il a été plus tard adapté et amélioré par mes soins pour pouvoir être joué directement dans le navigateur grâce à Javascript.</p>
             <a href="dtraparic.mmi-angouleme.com/speedup.html">Jouer ici</a>
           </div>
         </div>
-        <h4 class="accordeon__titre">Draw me a Ship</h4>
+        <h4 class="accordeon__titre"><span class="creations__projet creations__projet--dmas">Draw me a Ship</span></h4>
         <div class="accordeon__contentbox">
           <div class="accordeon__desc">
-            <p>Draw me a Ship a été le jeu créé pour la présentation du projet final d'<abbr title="Informatique & Sciences du Numérique">ISN</abbr> qui a reçu la note de 20 à l'épreuve du Bac. Dans ce jeu de Shoot Them Up, vous incarnez un vaisseau qui doit traverser des champs d'astéroides de plus en plus denses à l'aide de votre laser. Un système de monnaie et d'améliorations de votre vaisseau est présent pour vous aider.
+            <p><span class="creations__projet dmas">Draw me a Ship</span> a été le jeu créé pour la présentation du projet final d'<abbr title="Informatique & Sciences du Numérique">ISN</abbr> qui a reçu la note de 20 à l'épreuve du Bac. Dans ce jeu de Shoot Them Up, vous incarnez un vaisseau qui doit traverser des champs d'astéroides de plus en plus denses à l'aide de votre laser. Un système de monnaie et d'améliorations de votre vaisseau est présent pour vous aider.
             C'est avec ce projet que je me suis initié à la programmation orientée objet.</p>
           </div>
         </div>
-        <h4 class="accordeon__titre">Mastermind</h4>
+        <h4 class="accordeon__titre"><span class="creations__projet creations__projet--master">Mastermind</span></h4>
         <div class="accordeon__contentbox">
           <div class="accordeon__desc">
-            <p>Ce mastermind était un exercice de mon DUT MMI pour nous initier au Javascript. J'ai pu avec ce jeu m'entraîner sur différents aspects du Javascript comme <abbr title="Ensemble de nouvelles manières d'écriture Javascript facilitant sa lecture et son écriture">ES6</abbr> et les modules.</p>
+            <p>Ce <span class="creations__projet master">mastermind</span> était un exercice de mon DUT MMI pour nous initier au Javascript. J'ai pu avec ce jeu m'entraîner sur différents aspects du Javascript comme <abbr title="Ensemble de nouvelles manières d'écriture Javascript facilitant sa lecture et son écriture">ES6</abbr> et les modules.</p>
           </div>
         </div>
-        <h4 class="accordeon__titre">Galerie d'images éditable</h4>
+        <h4 class="accordeon__titre"><span class="creations__projet creations__projet--galimg">Galerie d'images éditable</span></h4>
         <div class="accordeon__contentbox">
           <div class="accordeon__desc">
-            <p>Cet exercice de synthèse sur le PhP permettait de prouver ses acquis sur l'organisation en <abbr title="Modèle-Vue-Controlleur : séparation des parties du code selon leurs finalités">MVC</abbr>, ainsi que l'utilisation d'outils PhP comme le package manager <cite>Composer</cite> et des bibliothèques de Laravel comme le moteur de templates <cite>Blade</cite></p>
+            <p>Cet exercice de synthèse sur le PhP permettait de prouver ses acquis sur l'organisation en <abbr title="Modèle-Vue-Controlleur : séparation des parties du code selon leurs finalités">MVC</abbr>, ainsi que l'utilisation d'outils PhP comme le package manager <cite>Composer</cite> et des bibliothèques de <cite>Laravel</cite> comme le moteur de templates <cite>Blade</cite></p>
           </div>
         </div>
       </div>
